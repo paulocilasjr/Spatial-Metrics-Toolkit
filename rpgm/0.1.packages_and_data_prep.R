d5f5f07a7a99759c4ec8788a9d216bf1884f71e9
ACSoupir <alex.soupir@jacks.sdstate.edu>
2024-12-09 10:16:20 -0500

    packages to install and example samples

---
+++ rpgm/0.1.packages_and_data_prep.R
@@ -0,0 +1,40 @@
+#packages
+renv::activate()
+
+install.packages(
+  c("spatstat", "dbscan", "dixon", #analyses
+    "magrittr", "data.table", "dplyr", "tibble", #data processing
+    "spatialTIME", #example data
+    "optparse" #allow Rscript to run as command line tool
+    ),
+  Ncpus = 4
+)
+
+# library(dplyr)
+# library(data.table)
+# library(spatialTIME)
+# #prepping data from the spatialTIME package
+# spatial_list = example_spatial
+# #get columns we want
+# sample_id = "deidentified_sample"
+# markers = c("CD3..FOXP3.", "CD3..CD8.", "CD3..CD8..FOXP3.",
+#             "CD3..PD1.", "CD3..PD.L1.", "CD8..PD1.", "CD3..CD8..PD.L1.",
+#             "CD3..Opal.570..Positive", "CD8..Opal.520..Positive",
+#             "PD1..Opal.650..Positive", "FOXP3..Opal.620..Positive",
+#             "PDL1..Opal.540..Positive")
+# tissue_col = "Classifier.Label"
+# #iterate over each sample
+# spatial_list2 = lapply(spatial_list, function(spat){
+#   spat %>% 
+#     mutate(x = (XMin + XMax)/2, #x and y are ranges
+#            y = (YMin + YMax)/2) %>%
+#     select(any_of(c(sample_id, "x", "y", markers, tissue_col))) #select our columns
+# })
+# 
+# #export to data/per-cell/
+# tmp = lapply(spatial_list2, function(spat){
+#   path = paste0("data/per-cell/",
+#                 unique(spat[[sample_id]]),
+#                 ".csv.gz")
+#   fwrite(spat, file = path, compress = 'gzip')
+# })
