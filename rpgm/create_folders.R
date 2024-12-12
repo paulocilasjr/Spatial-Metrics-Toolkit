cf66a8a2b2e39aa96fc0eab8785c5449b77a51cd
Jennifer Joseph <107996540+jenniferjose@users.noreply.github.com>
2024-12-12 13:49:33 -0500

    Update create_folders.R

--- rpgm/create_folders.R
+++ rpgm/create_folders.R
@@ -1,5 +1,3 @@
-#library(yaml)
-#ymlfile <- yaml::read_yaml("/Users/4479438/Documents/hackathon2024/Spati_met_toolkit/config.yml")
 createfolders <- function(yaml){ 
   output_path <- ymlfile$paths$output
   dir.create(output_path)
