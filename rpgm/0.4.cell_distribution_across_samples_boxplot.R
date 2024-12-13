42b0295dfab8a67b7d5019a9321b1e105da91005
joeyxu95 <69163158+joeyxu95@users.noreply.github.com>
2024-12-13 11:56:46 -0500

    Add files via upload

--- rpgm/0.4.cell_distribution_across_samples_boxplot.R
+++ rpgm/0.4.cell_distribution_across_samples_boxplot.R
@@ -4,59 +4,80 @@
 #libraries needed
 require(ggplot2)
 require(Polychrome) # generated colors
+require(ggpubr)
 cell_distribution_plot <- function(yaml_config=NULL){
+  # functions for barplot
+  box_plots <- function(data =NULL, 
+                        markers = NULL, 
+                        label =NULL){
+    plot_data <- matrix(0,nrow=nrow(data) * length(markers),ncol=3)
+    m <- 0
+    for(i in markers){
+      for (j in 1:nrow(data)){
+        m <- m + 1
+        plot_data[m,1] <- data[j,1] # sample name
+        plot_data[m,2] <- data[j,i] # Percentage of positive cells
+        plot_data[m,3] <- i # cell types
+      }
+    }
+    plot_data <- data.frame(plot_data)
+    plot_data[,1] <- gsub(".tif", "", plot_data[,1])
+    plot_data[,3] <- gsub("^X..", "",plot_data[,3])
+    plot_data[,3] <- gsub(".Cells", "",plot_data[,3])
+    colors <- createPalette(length(unique(plot_data[,1])), c("#2a6ebb", "#de3831", "#007367"), range = c(30, 80))
+    names(colors) <- unique(plot_data[,1])
+    theme_set(theme_bw())
+    ggplot(plot_data, aes(y=as.numeric(plot_data[,2]), x= plot_data[,3])) +
+      geom_boxplot(show.legend = F) +
+      geom_point(position = position_jitterdodge(jitter.width = 0, dodge.width = 0.5),
+                 aes(color = factor(plot_data[,1])), show.legend =T) +
+      labs(x = '', 
+           y = "Percentage of positive cells (%)",
+           color = "Sample ID"
+      ) +
+      scale_x_discrete(guide = guide_axis(angle = 45)) +
+      scale_color_manual(values=colors) +
+      theme(
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text.x = element_text(face='bold', size=10),
+        axis.text.y = element_text(face='bold', size=10),
+        axis.title.y = element_text(face='bold', size=10),
+        axis.title.x = element_text(face='bold', size=10),
+        legend.title = element_text(face='bold'),
+        legend.text = element_text(face='bold'))
+    
+    if (is.null(label)){
+    out1 <- paste0(outputFilePath, "/", "cell_distribution_acorss_sample_boxplot.pdf", sep="")
+    }else{
+      out1 <- paste0(outputFilePath, "/", label, "_cell_distribution_acorss_sample_boxplot.pdf", sep="")
+    }
+    ggsave(out1)
+    }
+  # summary file data read and generated the figures 
   if(is.null(yaml_config$variables$sample_id) & 
      is.null(yaml_config$variables$markers) & 
      is.null(yaml_config$paths$output) &
-     is.null(yaml_config$paths$sample)){
+     is.null(yaml_config$paths$sample) |
+     is.null(yaml_config$variables$tissue_class_label)){
     stop("sample_id, marker set, summary file path, and output path 
-    must be provided in yaml config file.", call.=FALSE)
-  }
+    must be provided or tissue_class_label in optional in yaml config file.", call.=FALSE)}
   summary_path <- yaml_config$paths$sample
   marker_set <- yaml_config$variables$markers
   sample_col <- yaml_config$variables$sample_id
   outputFilePath = yaml_config$paths$output
   data <- read.csv(file = summary_path, header = TRUE)
-  marker_set1 <- gsub(".Positive", "", marker_set)
-  marker_set1 <- paste0("X..", marker_set1, ".Positive.Cells")
-  data <- data[, c(sample_col, marker_set1)]
-  plot_data <- matrix(0,nrow=nrow(data) * length(marker_set1),ncol=3)
-  m <- 0
-  for(i in marker_set1){
-    for (j in 1:nrow(data)){
-      m <- m + 1
-      plot_data[m,1] <- data[j,1] # sample name
-      plot_data[m,2] <- data[j,i] # Percentage of positive cells
-      plot_data[m,3] <- i # cell types
-    }
+  # marker_set1 <- gsub(".Positive", "", marker_set)
+#  marker_set1 <- paste0("X..", marker_set1, ".Positive.Cells")
+  marker_set1 <- paste0("X..", marker_set, ".Cells")
+  if (is.null(yaml_config$variables$tissue_class_label)){
+    data1 <- data[, c(sample_col, marker_set1)]
+    box_plots(data1, marker_set1, label = NULL)
+  } else {
+    class_label <- yaml_config$variables$tissue_class_label
+   for (i in unique(data[,class_label])){
+     data1 <- data[data[,class_label]== i, c(sample_col, marker_set1)]
+     box_plots(data1, marker_set1, label = i)
+   } 
   }
-  plot_data <- data.frame(plot_data)
-  plot_data[,1] <- gsub(".tif", "", plot_data[,1])
-  plot_data[,3] <- gsub("^X..", "",plot_data[,3])
-  plot_data[,3] <- gsub("..Positive.Cells", "",plot_data[,3])
-  marker_set1 <- gsub("^X..", "",marker_set1)
-  marker_set1 <- gsub("..Positive.Cells", "",marker_set1)
-  colors <- createPalette(length(marker_set1), c("#2a6ebb", "#de3831", "#007367"), range = c(30, 80))
-  names(colors) <- marker_set1
-  theme_set(theme_bw())
-  ggplot(plot_data, aes(y=as.numeric(plot_data[,2]), x= plot_data[,1], 
-                        fill=factor(plot_data[,3], levels = marker_set1))) +
-    geom_bar(position="stack",stat = "identity") +
-    labs(x = '', 
-         y = "Percentage of positive cells (%)",
-         fill = "Cell type"
-    ) +
-    scale_x_discrete(guide = guide_axis(angle = 90)) +
-    scale_fill_manual(values=colors) +
-    theme(
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(),
-      axis.text.x = element_text(face='bold', size=10),
-      axis.text.y = element_text(face='bold', size=10),
-      axis.title.y = element_text(face='bold', size=10),
-      axis.title.x = element_text(face='bold', size=10),
-      legend.title = element_text(face='bold'),
-      legend.text = element_text(face='bold'))
-  out1 <- paste0(outputFilePath, "/", "cell_distribution_acorss_sample_boxplot.pdf", sep="")
-  ggsave(out1)
 }
